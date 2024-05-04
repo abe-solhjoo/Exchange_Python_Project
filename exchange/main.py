a1bbbebe7260a693ff05cf525f003d0b76dbb769
Ebrahim <eb.solhjoo@gmail.com>
2024-05-04 17:47:24 +0330

    add archive and json

--- exchange/main.py
+++ exchange/main.py
@@ -1,11 +1,28 @@
 import requests
 from config import URL
+import json
+from datetime import datetime
 
 
 def get_rates():
     response = requests.get(URL)
-    print(response.text)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return response.status_code
+
+
+def archive(rates):
+    with open(f'archive/{date_of_now()}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+
+def date_of_now():
+    now = datetime.now()
+    str_time = f'{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}'
+    return str_time
 
 
 if __name__ == "__main__":
-    get_rates()
+    rsp = get_rates()
+    print(rsp)
+    archive(rsp)
